9021b81787dd8d0995bcd440d9ef09f33e748a25
git <eae966@gmail.com>
2023-03-05 22:56:09 +0330

    Some Updates.

--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
-func main() {
-	source := flag.String("s", "", "Source file path")
-	destination := flag.String("o", "", "Destination file path")
-
-	flag.Parse()
-
-	fmt.Printf("Source file path: %s\n", *source)
-	fmt.Printf("Destination file path: %s\n", *destination)
+type stateData struct {
+	SPtr string `json:"s_ptr"`
+	OPtr string `json:"o_ptr"`
 }
+
+func loadStateFromJSON(path string) (*stateData, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var state stateData
+	err = json.NewDecoder(file).Decode(&state)
+	if err != nil {
+		return nil, err
+	}
+
+	return &state, nil
+}
+
+func main() {
+	// Check if state file exists
+	if _, err := os.Stat("state.json"); err == nil {
+		// State file exists, load previous state
+		state, err := loadStateFromJSON("state.json")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Println("Previous state loaded:")
+		fmt.Printf("-s=%s\n", state.SPtr)
+		fmt.Printf("-o=%s\n", state.OPtr)
+
+	}

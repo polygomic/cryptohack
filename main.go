94f03e782c1e8a317c5a6a346ca3ef0a1d03cad5
git <eae966@gmail.com>
2023-03-05 22:59:05 +0330

    Bug fixes.

--- main.go
+++ main.go
@@ -4,42 +4,63 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
 )
 
-type stateData struct {
-	SPtr string `json:"s_ptr"`
-	OPtr string `json:"o_ptr"`
-}
-
-func loadStateFromJSON(path string) (*stateData, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var state stateData
-	err = json.NewDecoder(file).Decode(&state)
-	if err != nil {
-		return nil, err
-	}
-
-	return &state, nil
+type State struct {
+	SPtr string `json:"s"`
+	OPtr string `json:"o"`
 }
 
 func main() {
-	// Check if state file exists
-	if _, err := os.Stat("state.json"); err == nil {
-		// State file exists, load previous state
-		state, err := loadStateFromJSON("state.json")
+	// Parse command line arguments
+	sPtr := flag.String("s", "", "source file path")
+	oPtr := flag.String("o", "", "output file path")
+	flag.Parse()
+
+	// Check if both -s and -o are provided
+	if *sPtr == "" || *oPtr == "" {
+		state, err := readState()
+		if err != nil {
+			log.Fatalln("Both -s and -o options are required")
+		}
+		sPtr = &state.SPtr
+		oPtr = &state.OPtr
+	} else {
+		state := State{*sPtr, *oPtr}
+		err := writeState(state)
 		if err != nil {
 			log.Fatalln(err)
 		}
-
-		fmt.Println("Previous state loaded:")
-		fmt.Printf("-s=%s\n", state.SPtr)
-		fmt.Printf("-o=%s\n", state.OPtr)
-
 	}
+
+	// Print the values of -s and -o
+	fmt.Printf("-s flag value: %s\n", *sPtr)
+	fmt.Printf("-o flag value: %s\n", *oPtr)
+}
+
+func readState() (State, error) {
+	data, err := ioutil.ReadFile("state.json")
+	if err != nil {
+		return State{}, err
+	}
+	var state State
+	err = json.Unmarshal(data, &state)
+	if err != nil {
+		return State{}, err
+	}
+	return state, nil
+}
+
+func writeState(state State) error {
+	data, err := json.MarshalIndent(state, "", "    ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("state.json", data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
